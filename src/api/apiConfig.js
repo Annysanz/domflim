1b57ac4b7609587b736ac146cc3a7b295a0eff3f
Gerardo <=>
2022-02-12 09:09:09 -0600

    Listo para Empezr

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,7 +1,7 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
     apiKey: 'be8625e8961ba0c2d954cfc358a6364f',
-    originalImage: (imgPath) => `https://image.tmdb.org/t/p/w500/original/${imgPath}`,
+    originalImage: (imgPath) => `https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
 
