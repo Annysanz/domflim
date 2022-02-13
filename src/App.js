e1d1928232c8aafc86720f13dbfbb09d302250e4
Gerardo <=>
2022-02-13 11:55:17 -0600

    pag de pre-inicio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import React, {useState, useEffect} from 'react';
 import 'swiper/swiper.min.css';
 import './assets/boxicons-2.0.7/css/boxicons.min.css';
 import './App.scss';
+
 
 import { BrowserRouter, Route } from 'react-router-dom';
 
@@ -8,10 +10,25 @@ import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 
 import Routes from './config/Routes';
+import PreLoader from './components/animation/PreLoader';
 
 function App() {
+    const [loading, setLoading] = useState(false);
+     
+    useEffect(() => {
+       setLoading(true)
+       setTimeout(() => {
+        setLoading(false)   
+       }, 8000)
+    }, [])
+    
     return (
         <BrowserRouter>
+                            {
+                    loading ?
+                     <PreLoader />
+                    :
+            
             <Route render={props => (
                 <>
                     <Header {...props}/>
@@ -19,6 +36,7 @@ function App() {
                     <Footer/>
                 </>
             )}/>
+}
         </BrowserRouter>
     );
 }
---
+++ src/components/animation/PreLoader.js
@@ -0,0 +1,59 @@
+import React, {useEffect, useState } from "react";
+import "./PreLoader.css";
+import dom from './dom.png';
+
+function PreLoader() {
+    const [data, setData] = useState([]);
+    const [loading, setloading] = useState(undefined);
+    const [completed, setcompleted] = useState(undefined);
+
+    useEffect(() => {
+      setTimeout(() => {
+         fetch("https://jsonplaceholder.typicode.com/posts")
+         .then((response) => response.json())
+         .then((json) => {
+             console.log(json);
+             setData(json);
+             setloading(true);
+
+            setTimeout(() => {
+                setcompleted(true);
+            }, 0);
+
+         });
+
+      }, 1500);
+
+    }, []);
+   
+    return (
+      <>
+
+        {!completed ? (
+          <>
+          {!loading ? (
+
+            <div className="box">
+              <span>loading...</span>
+               <div className="half-spinner"></div>
+            </div>
+        
+        ) : (
+           <div className="completed">&#x2713;</div>
+
+        )}
+      </>
+
+    ) : (
+        <>
+            <img src={ dom } className="domflim"/>
+            <span className="legend">El cine en tu hogar</span>  
+        </>
+
+    )}
+
+    </>
+   );
+}
+
+export default PreLoader;
