7572663048cd06bc392d7e1a58f198ddcff34d62
Gerardo <=>
2022-02-15 16:22:57 -0600

    logi

--- src/components/login/Base.js
+++ src/components/login/Base.js
@@ -3,6 +3,8 @@ import "./fireStyle.css";
 import Button from "../button/Button";
 import Input from "../input/Input";
 import { useState } from "react";
+
+
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -10,13 +12,29 @@ import {
   signOut,
 } from "firebase/auth";
 import { auth } from "./firebase.js";
+import Particles from "react-tsparticles";
 
 export function Base() {
+  
+  const particlesInit = (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+  
+  
+  
+  
   const [registerEmail, setRegisterEmail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
   const [user, setUser] = useState({});
+
 
   onAuthStateChanged(auth, (currentUser) => {
     setUser(currentUser);
@@ -55,15 +73,101 @@ export function Base() {
   return (
     <>
       <div className="wrapper fadeInDown">
+
+      <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: 'oranges',
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 10,
+              opacity: 0.2,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: false,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+       <div className="loginer">    
         <div>
           <h3 className="titles"> Registrar Usuario </h3>
           <Input
+            id="email"
             placeholder="Email..."
             onChange={(event) => {
               setRegisterEmail(event.target.value);
             }}
           />
           <Input
+             id="password"
             placeholder="Password..."
             onChange={(event) => {
               setRegisterPassword(event.target.value);
@@ -78,12 +182,14 @@ export function Base() {
         <div>
           <h3 className="titles">Entrar</h3>
           <Input
+            id='email'
             placeholder="Email..."
             onChange={(event) => {
               setLoginEmail(event.target.value);
             }}
           />
           <Input
+            id='password'
             placeholder="Password..."
             onChange={(event) => {
               setLoginPassword(event.target.value);
@@ -94,11 +200,13 @@ export function Base() {
             Login
           </Button>
         </div>
+
         <h4 className="titles">Usuario Actual:</h4>
         {user?.email}
         <Button onClick={logout} className="login-style">
           Salir
         </Button>
+      </div>
       </div>
     </>
   );
