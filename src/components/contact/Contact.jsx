21fe29c26fe06cca9492fedfcc7f60580c3fc8a8
Annysanz <annysanz17@gmail.com>
2022-02-18 12:30:42 +0100

    contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,25 +3,25 @@ import "./contact.scss";
 
 const Contact = () => {
   return (
-    <div className="contact">
-        <div class="col-md-7">
+    <div classNameName="contact">
+        <div className="col-md-7">
                 <h1>Contáctanos</h1>
                 <br></br>
                 <form>
-                    <div class=""> 
-                    <label for="" class="">Nombre </label>
+                    <div className=""> 
+                    <label for="" className="">Nombre </label>
                       <br></br><br></br>
-                    <input type="text" class="" placeholder="Your name"/> 
+                    <input type="text" className="" placeholder="Your name"/> 
                       <br></br> <br></br>
-                    <label for="" class=""> Email</label>
+                    <label for="" className=""> Email</label>
                       <br></br><br></br>
-                    <input type="email" class="" placeholder="Your email"/> 
+                    <input type="email" className="" placeholder="Your email"/> 
                       <br></br> <br></br>
-                    <label for="" class=""> Mensaje </label>
+                    <label for="" className=""> Mensaje </label>
                       <br></br><br></br>
                     <textarea cols="31" rows="8" placeholder="Escribe tu mensaje"> </textarea>
                       <br></br><br></br>
-                    <input type="submit" value="Enviar" class="btn btn-block btn-primary"/>
+                    <input type="submit" value="Enviar" className="btn btn-block btn-primary"/>
                       </div>
                 </form>
        </div>
